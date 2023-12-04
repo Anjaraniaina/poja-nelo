b89e3560a386b22decf32af8b78119d75334e317
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-12-04 09:38:59 +0300

    chore: setup done

---
+++ src/main/java/com/home/poja/service/event/UuidCreatedService.java
@@ -0,0 +1,26 @@
+package com.home.poja.service.event;
+
+import com.home.poja.PojaGenerated;
+import com.home.poja.endpoint.event.gen.UuidCreated;
+import com.home.poja.repository.DummyUuidRepository;
+import com.home.poja.repository.model.DummyUuid;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@PojaGenerated
+@Service
+@AllArgsConstructor
+@Slf4j
+public class UuidCreatedService implements Consumer<UuidCreated> {
+
+  private final DummyUuidRepository dummyUuidRepository;
+
+  @Override
+  public void accept(UuidCreated uuidCreated) {
+    var dummyUuid = new DummyUuid();
+    dummyUuid.setId(uuidCreated.getUuid());
+    dummyUuidRepository.save(dummyUuid);
+  }
+}
